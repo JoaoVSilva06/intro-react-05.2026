b1bf907f7a724ca3106df908d3cc391c2dc77ca2
JoaoVSilva06 <joaooviitor2006@gmail.com>
2026-05-16 00:51:59 +0000

    Adicionado useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import {Hello} from './components/Hello.jsx';
 
 
 function App() {
+
     // inicializei um estado com o valor inicial false
     // desconstrucao de array
     const [userState, setUserState] = useState(false);
     // neste momento, userState está com valor false
+
+
+    // useEffect permite controlar/detectar o ciclo de vida de um componente
+    useEffect(()=>{
+        console.log("O componente foi montado");
+    }, []); // [] = array vazio é o onLoad do componente
+    // dispara uma função quando muda o valor de userState
+    useEffect(()=>{
+        console.log("O estado do usuario mudou para: ", userState)
+    },[userState]);
+
 
     /*
     setTimeout(function(){
